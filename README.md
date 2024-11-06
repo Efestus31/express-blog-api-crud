1498c81188f4c8be9737ff35151fc7a11766da9f
Efestus31 <biagiohd@gmail.com>
2024-11-06 13:25:21 +0100

    add put and delete routes add some coments update readme

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ Bonus
 * persistenza: come visto in classe usa il modulo fs per salvare la nuova array nel file dopo aver inserito un nuovo elemento grazie alla rotta store creata
 * e se volessi aggiornare un post? sapresti implementare la rotta update per aggiornare una risorsa esistente?
 Alla fine dovrete usare postman come visto in classe per testare tutti i vostri endpoint.
+
+## express-blog-api-crud (update)
+Per il nostro blog, concentriamoci sul creare 2 rotte:
+/:id [PUT] - rotta update del crud che riceverà dei dati e modificherá un post esistente. Questa dovrà riceve i dati in formato json e dovrà restituire il json dell’elemento appena creato.
+/:id [DELETE] - rotta destroy del crud che dovrà restituire un 404 nel caso non sia stato trovato un post corrispondente.
+Restituire come responso la nuova array escluso il post eliminato
+Testare le rotte tramite Postman.
+
--- routes/posts.js
+++ routes/posts.js
@@ -3,17 +3,27 @@ const express = require('express')
 //define the router instance
 const router = express.Router()
 const postController = require('../controller/postController.js')
-
 //define all routes here
 
+//API CRUD
+
 //Post API
-router.get('/', postController.index)
+ // (R) index shows all posts
+router.get('/', postController.index) // /posts
 
-//show only the single(selected resources)
-router.get('/:slug', postController.show)
+// (R) show only the single(selected resources)
+router.get('/:slug', postController.show) //posts/tortini-verdure
 
-//crea un novo oggetto
-router.post('/', postController.store)
+//(C) create a new resource inside of the array 
+router.post('/', postController.store) // /posts
+
+//(U) update an existing resource
+router.put('/:slug', postController.update)
+
+ //(D) delete an existing resource
+ router.delete('/:slug', postController.destroy)
+
+
 
 
 //export the router instance
